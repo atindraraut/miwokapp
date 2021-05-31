b3c0f5bf21ea911a23771a6c65ef8f09fc737395
atindraraut <atindraraut80@gmail.com>
2021-06-01 01:13:49 +0530

    numbers updated

--- app/src/main/java/com/example/miwokapp/numbersActivity.java
+++ app/src/main/java/com/example/miwokapp/numbersActivity.java
@@ -3,6 +3,11 @@ package com.example.miwokapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class numbersActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class numbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers2);
+
+        ArrayList<String> numbersarray = new ArrayList<String>();
+        numbersarray.add("one");
+        numbersarray.add("two");
+        numbersarray.add("three");
+        numbersarray.add("four");
+        numbersarray.add("five");
+        numbersarray.add("six");
+        numbersarray.add("seven");
+        numbersarray.add("eight");
+        numbersarray.add("nine");
+        numbersarray.add("ten");
+
+        int counter =0;
+        while(0<numbersarray.size()){
+            Log.i("numbersActivity", numbersarray.get(counter));
+            LinearLayout mainview = (LinearLayout) findViewById(R.id.mainview);
+            TextView subview= new TextView(this);
+            subview.setText(numbersarray.get(counter));
+            subview.setTextSize(32);
+            mainview.addView(subview);
+            counter++;
+
+        }
+
     }
 }
