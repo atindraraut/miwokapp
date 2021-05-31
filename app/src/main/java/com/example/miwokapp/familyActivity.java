68eb3cd90efaba1e1aa177773d5dc538bb217278
atindraraut <atindraraut80@gmail.com>
2021-05-31 22:28:49 +0530

    numbers activiti updated

---
+++ app/src/main/java/com/example/miwokapp/familyActivity.java
@@ -0,0 +1,14 @@
+package com.example.miwokapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class familyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+    }
+}
